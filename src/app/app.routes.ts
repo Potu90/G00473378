d8b5c40158e292b53f5c396bac27ebfda3bbd787
Potu90 <josuton@gmail.com>
2026-04-13 13:45:24 +0000

    Fix duplicated home route and add student ID to Home Page header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
   },
   {
     path: 'movie-details/:id',
-    loadComponent: () => import('./pages/movie-details/movie-details.page').then( m => m.MovieDetailsPage)
+    loadComponent: () => import('./pages/movie-details/movie-details.page').then(m => m.MovieDetailsPage)
   },
   {
     path: 'details/:id',
-    loadComponent: () => import('./pages/details/details.page').then( m => m.DetailsPage)
+    loadComponent: () => import('./pages/details/details.page').then(m => m.DetailsPage)
   },
   {
     path: 'favourites',
-    loadComponent: () => import('./pages/favourites/favourites.page').then( m => m.FavouritesPage)
+    loadComponent: () => import('./pages/favourites/favourites.page').then(m => m.FavouritesPage)
   },
 ];
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -27,14 +27,23 @@ export class HomePage implements OnInit {
   }
 
   getTrendingMovies() {
-    //Call TMBD to get today's trending movies
+    //Call web page to get today's trending movies
     this.http.get<any>('https://api.themoviedb.org/3/trending/movie/day?api_key=YOUR_API_KEY').
     subscribe(data => {this.movies = data.results;
     });
   }
 
   searchMovies() {
-    
+    //If search is empty, show trending movies
+    if (this.searchText == '') {
+      this.getTrendingMovies();
+      return;
+    }
+
+    //Call it with the search text and wait for the response
+    this.http.get<any>(`https://api.themoviedb.org/3/search/movie?query=${this.searchText}&api_key=YOUR_API_KEY`).
+    subscribe(data => {this.movies = data.results;
+    });
   }
 
   goToMovieDetails(movie: any) {
