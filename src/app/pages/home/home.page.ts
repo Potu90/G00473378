b1d0d588f204c18b2c2f049ccc65c02376e18de6
Potu90 <josuton@gmail.com>
2026-04-14 13:56:56 +0000

    Add API key to the methods

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonButton, IonLabel, IonItem } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,27 +9,30 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSearchbar, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSearchbar, IonButton, IonLabel, IonItem]
 })
 export class HomePage implements OnInit {
 
-  //Array to store the list of movies
-  movies: any[] = [];
+  movies: any[] = [];                                  //Array to store the list of movies
+  searchText: string = '';                             //Variable to store the search text entered by the user
+  apiKey: string = '04b4a3b05536f796e2be2bb50fb5c234'; //API key to authenticate requests 
 
-  //Variable to store the search text entered by the user
-  searchText: string = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     //Show Trending Movies at the beggining
     this.getTrendingMovies();
+
+    console.log('API Key: ' + this.apiKey);
   }
 
   getTrendingMovies() {
     //Call web page to get today's trending movies
-    this.http.get<any>('https://api.themoviedb.org/3/trending/movie/day?api_key=YOUR_API_KEY').
+    this.http.get<any>(`https://api.themoviedb.org/3/trending/movie/day?api_key=${this.apiKey}`).
     subscribe(data => {this.movies = data.results;
+    //Test: print the data received from the API
+    console.log(data);
     });
   }
 
@@ -41,7 +44,9 @@ export class HomePage implements OnInit {
     }
 
     //Call it with the search text and wait for the response
-    this.http.get<any>(`https://api.themoviedb.org/3/search/movie?query=${this.searchText}&api_key=YOUR_API_KEY`).
+    this.http.get<any>(`https://api.themoviedb.org/3/search/movie?query=${this.searchText}&api_key=${this.apiKey}`).
+
+
     subscribe(data => {this.movies = data.results;
     });
   }
